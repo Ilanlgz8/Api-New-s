ed61dc0c672e80537da14c9572d600e828d8c65b
ilanlangelez <ilanlgz@Mac.lan>
2026-05-27 23:53:20 +0200

    chore: commit remaining files and build artifacts

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"7nqqFHg8xnH8Xc9z0/02cMz150ISVdB1A3Yqw+4OVMQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DMSdsmMjMyTBBKQrFZC6rPgGKanF9V7d2Bc9h7W6xzg=\"\n}"
